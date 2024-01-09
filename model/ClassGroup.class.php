f168f4a20f42045bedbf1573388537af36733387
Mazine <mazine.meriche@etu.univ-grenoble-alpes.fr>
2024-01-09 16:37:37 +0100

    Ajout fonction pour chercher un groupe via un prof ou un élève (à tester)

--- model/ClassGroup.class.php
+++ model/ClassGroup.class.php
@@ -10,13 +10,85 @@ class ClassGroup{
         $this->owner = $owner;
     }
 
-
     //TODO
     public function create(){}
 
 
     //TODO
     public function insertStudent(){}
+
+    //Retourne la liste des groupes de classe d'un professeur
+    //TODO Tester cette fonction
+    public static function getClassGroupFromTeacher(Teacher $teacher) : array {
+
+        $dao = DAO::get();
+        $teacherId = $teacher->getId();
+        $query = "SELECT id FROM ClasseProf p, Class c WHERE classId = id and teacherId = ?";
+
+        $table = $dao->query($query,$teacherId);
+
+        if(count($table) == 0 ){
+            throw new Exception("Ce professeur n'a pas de groupe de classe ! ");
+        }
+        
+        $result = [];
+        $studentsId = [];
+
+        foreach($table as $classId){
+
+            $classGroup = new ClassGroup($teacher);
+            $classGroup->id = $classId['id'];
+            $query = "SELECT studentId FROM Class c, StudentClass s WHERE id = classId";
+            $studentsId = $dao->query($query);
+
+            foreach($studentsId as $studentId){
+
+                $student = Student::readStudent($studentId['studentId']);
+                $classGroup->students[] = $student;
+
+            }
+
+            $result[] = $classGroup;
+
+        }
+
+        return $result;
+
+    }
+
+    public static function getClassGroupFromStudent(Student $student) : ClassGroup{
+
+        $dao = DAO::get();
+        $studentId = $student->getId();
+        $query = "SELECT id FROM StudentClass s, Class c WHERE classId = id and studentId = ?";
+        
+        $table = $dao->query($query,$studentId);
+
+        if(count($table) == 0 ){
+            throw new Exception("Cet élève n'a pas de groupe de classe ! ");
+        }
+
+        $groupId = $table[0]['id'];
+
+        $query = "SELECT teacherId FROM ClasseProf p, Class c WHERE classId = id";
+        $teacherIdRes = $dao->query($query);
+        $teacherId = $teacherIdRes[0]['teacherId'];
+        $classGroup = new GroupClass(Teacher::readTeacher($teacherId));
+        $classGroup->id = $groupId;
+
+        $query = "SELECT studentId FROM Class c, StudentClass s WHERE id = classId";
+        $table = $dao->query($query);
+
+        foreach($table as $student){
+            $classGroup->students[] = Student::readStudent($student['studentId']);
+        }
+
+        return $classGroup;
+
+
+
+
+    }
 }
 
 ?>
