13d5a83c8e5740ead763fd581b10f05c94ed2b7f
MatysMarrot <marrotm38@gmail.com>
2024-01-10 11:39:12 +0100

    Ajout landing page

---
+++ view/landingpage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Scooby Gang" />
+    <link rel="stylesheet" type="text/css" href="style/login.css">
+    <title>Acceuil</title>
+</head>
+<body id="">
+    <header class="connectionpage">
+        <h1>Chronotravel</h1>
+    </header>
+    <main class="landingpage">
+        <h2>
+            Plongez dans l'aventure captivante de notre jeu éducatif et revisitez l'histoire de manière ludique, préparez-vous brillamment pour votre brevet tout en défiant votre esprit historique!
+        </h2>
+        <br>
+        <br>
+        <a href="login.php" class="button">Jouer maintenant !</a>
+    </main>
+    <?php include(__DIR__.'/footer.viewpart.html')?>
+</body>
+</html>
