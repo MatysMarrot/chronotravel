56cdba91aab3f8e0555ce679bc5e3dfd28e4e9ee
Yaniissou <yanissoumc@gmail.com>
2024-01-10 14:21:17 +0100

    session change

--- controler/landing.ctrl.php
+++ controler/landing.ctrl.php
@@ -2,7 +2,7 @@
 //Inclure le php
 include_once(__DIR__."/../framework/view.class.php");
 
-if(isset($_SESSION['login'])){
+if($_SESSION['login'] == true){
     include("view/student.lobby.view.php");
 } else{
     include("view/landingpage.php");
