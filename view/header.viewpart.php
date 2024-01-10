6128f7d4a7dcecb438fdf9b21ac3a7157f829d31
DUCHOSAL Jolan <duchosaj@hostname.iut2.upmf-grenoble.fr>
2024-01-10 09:23:33 +0100

    started home

---
+++ view/header.viewpart.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a class="actif" href="#">ACCUEIL <img id="home" src="img/accueil.png" alt="Accueil"></a></li>
+            <li><a href="#">MON PROFIL</a></li>
+            <li><a href="#">MES TENUES</a></li>
+            <li><a href="#">CONTACT</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ view/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+  <html lang="fr">
+    <head>
+      <meta charset="UTF-8">
+      <title></title>
+      <link rel="stylesheet" href="style/header_style.css"> 
+    </head>
+
+    <body>
+    <?php include(__DIR__.'/header.viewpart.php'); ?>
+
+    </body>
+  </html>
