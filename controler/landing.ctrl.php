ef90c237eb8bd3e26328d004dc0ff1506a4cc312
Yaniissou <yanissoumc@gmail.com>
2024-01-10 14:22:18 +0100

    changed path of include

--- controler/landing.ctrl.php
+++ controler/landing.ctrl.php
@@ -3,7 +3,7 @@
 include_once(__DIR__."/../framework/view.class.php");
 
 if($_SESSION['login'] == true){
-    include("view/student.lobby.view.php");
+    include("../view/student.lobby.view.php");
 } else{
-    include("view/landingpage.php");
+    include("../view/landingpage.php");
 }
