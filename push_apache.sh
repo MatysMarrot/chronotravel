86c1f24d30aa03f9cedb2d7140ca021c36d5cc61
Yanissou <yanissoumc@gmail.com>
2024-01-11 12:36:04 +0100

    First draft on server push script

---
+++ push_apache.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#TODO : add le fichier au gitignore psq ça doit pas arriver sur le serveur.
+# INFOS MACHINE
+user="atikr"
+host="192.168.14.112"
+port="22"  # Par défaut, le port SSH est 22, à vérifier si c'est bien le port utilisé, mais ça m'a l'air bon.
+#         Faut une clé ssh dcp, pas de mdp dans un tel fichier
+
+# GETTING "repo_local" AS CURRENT REPOSITORY (SCRIPT MUST BE EXECUTED IN THE CURRENT REPO) (might change in the future)
+repo_local="."
+
+# GETTING THE CURRENT BRANCH
+branche_actuelle=$(git -C "$repo_local" rev-parse --abbrev-ref HEAD)
+
+# On met les fichiers sur root du coup ? Ou dans les fichiers de l'utilisateur atikr? à clarifier avec Reda
+
+# Chemin sur la machine distante où le référentiel sera copié
+repo_distante="~/repositories/chronotravel/$branche_actuelle"
+
+#  S'assurer qu'on ait bien mit rsync sur la machine qu'on utilise
+# Utiliser rsync pour copier le référentiel vers le dossier de la branche
+rsync -avz -e "ssh -p $port" "$repo_local/" "$user@$host:$repo_distante/"
+
+# Vérifier si la commande rsync s'est bien exécutée
+if [ $? -eq 0 ]; then
+    echo "Copie réussie vers la machine distante dans le dossier de la branche $branche_actuelle."
+else
+    echo "Échec de la copie vers la machine distante."
+fi
