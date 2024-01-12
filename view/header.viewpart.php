3b1bf7967845f3bccd81a1ed14a4a068c7ef2c4b
DUCHOSAL Jolan <duchosaj@hostname.iut2.upmf-grenoble.fr>
2024-01-12 15:23:46 +0100

    First commitgit add .git add .

--- view/header.viewpart.php
+++ view/header.viewpart.php
@@ -1,10 +1,26 @@
 <header>
     <nav>
         <ul>
-            <li><a class="actif" href="#">ACCUEIL <img id="home" src="img/accueil.png" alt="Accueil"></a></li>
-            <li><a href="#">MON PROFIL</a></li>
-            <li><a href="#">MES TENUES</a></li>
-            <li><a href="#">CONTACT</a></li>
+            <li <?= ($currentPage == 'home') ? 'class="active"' : ''; ?>>
+                <a href="home.php"> <i class="material-symbols-outlined">Home</i></a>
+                <span class="tooltip_text">ACCUEIL</span>
+            </li>
+            <li <?= ($currentPage == 'profile') ? 'class="active"' : ''; ?>>
+                <a href="profile.php"><i class="material-symbols-outlined">Person</i></a>
+                <span class="tooltip_text">MON PROFIL</span>
+            </li>
+            <li <?= ($currentPage == 'outfit') ? 'class="active"' : ''; ?>>
+                <a href="outfit.php"> <i class="material-symbols-outlined">Checkroom</i></a>
+                <span class="tooltip_text">MES TENUES</span>
+            </li>
+            <li <?= ($currentPage == 'logout') ? 'class="active"' : ''; ?>>
+                <a href="logout.php"> <i class="material-symbols-outlined">Logout</i></a>
+                <span class="tooltip_text">DÉCONNEXION</span>
+            </li>
         </ul>
     </nav>
+    <div>
+        <img src="img/chrono_coin.png" alt="ChronoCoins">
+        <span>12345</span>
+    </div>
 </header>
--- view/home.php
+++ view/home.php
@@ -3,11 +3,15 @@
     <head>
       <meta charset="UTF-8">
       <title></title>
-      <link rel="stylesheet" href="style/header_style.css"> 
+      <link rel="stylesheet" href="style/header_style.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     </head>
 
     <body>
-    <?php include(__DIR__.'/header.viewpart.php'); ?>
+    <?php
+      $currentPage = 'home';
+      include(__DIR__.'/header.viewpart.php');
+    ?>
 
     </body>
   </html>
