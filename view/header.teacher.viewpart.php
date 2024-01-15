513a9a3723adbab51d14af127368c5a4c06e5573
JAYET-GRIFFON Xavier <xavier.jayet-griffon@etu.univ-grenoble-alpes>
2024-01-15 11:00:25 +0100

    header teacher new css

--- view/header.teacher.viewpart.php
+++ view/header.teacher.viewpart.php
@@ -1,13 +1,13 @@
 <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" type="text/css" href="../view/style/header.teacher.css">
 </head>
 <header class="header_teacher">
         <nav>
             <ul class="ul_horizontal">
-                    <li ><a class="actif" href="#">ACCUEIL</a></li>
-                    <li><a href="#">MON PROFIL</a></li>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">CONTACT</a></li>
+                    <li ><a <?= ($currentPage == 'logout') ? 'class="active"' : ''; ?> href="#">ACCUEIL</a></li>
+                    <li><a <?= ($currentPage == 'logout') ? 'class="active"' : ''; ?> href="#">MON PROFIL</a></li>
+                    <li><a <?= ($currentPage == 'logout') ? 'class="active"' : ''; ?> href="#">FAQ</a></li>
+                    <li><a <?= ($currentPage == 'logout') ? 'class="active"' : ''; ?> href="#">CONTACT</a></li>
             </ul>
         </nav>
 </header>
