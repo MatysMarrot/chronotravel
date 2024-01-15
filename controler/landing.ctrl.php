cf8dc3edef5ab65ecc3085f609c58a433033a3e6
MatysMarrot <marrotm38@gmail.com>
2024-01-15 15:43:49 +0100

    Controler of the landing page

--- controler/landing.ctrl.php
+++ controler/landing.ctrl.php
@@ -5,3 +5,10 @@ include_once(__DIR__."/../framework/view.class.php");
 include(__DIR__."/../view/landingpage.php");
 
 var_dump($_SESSION);
+
+// Controler
+$view = new View();
+$outgoing = "landingpage.view.php";
+
+$view->display("../view/".$outgoing);
+?>
