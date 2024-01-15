c6b601f774f2ae2b62f9f891c11f3ae7be04d4ab
JAYET-GRIFFON Xavier <xavier.jayet-griffon@etu.univ-grenoble-alpes>
2024-01-15 09:47:35 +0100

    fixe header css style sheet

--- view/header.viewpart.php
+++ view/header.viewpart.php
@@ -1,3 +1,6 @@
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
 <header>
     <nav>
         <ul>
