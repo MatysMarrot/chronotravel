bf52af3362d34321779b3ab952cf432b03cf8901
Yanissou <yanissoumc@gmail.com>
2024-01-16 13:54:49 +0100

    blank page, trying to debug

--- test/Question.test.php
+++ test/Question.test.php
@@ -7,5 +7,7 @@ require_once(__DIR__ . '/../model/Question.class.php');
 
 //Récupérer le nombre de questions en DB
 $size = Question::getQuestionsSize();
-echo "size : ";
-echo $size;
+print ("size : ");
+print ($size);
+
+?>
