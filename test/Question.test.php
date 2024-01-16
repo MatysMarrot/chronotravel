d999de92d621af54947d1392293df55f92c1c5e6
Yanissou <yanissoumc@gmail.com>
2024-01-16 13:44:25 +0100

    Question size, some tests

--- test/Question.test.php
+++ test/Question.test.php
@@ -2,5 +2,9 @@
 require_once(__DIR__ . '/../model/Question.class.php');
 
 //Création d'une question aléatoire
-$question = Question::getRandomQuestion();
-echo $question->display();
+//$question = Question::getRandomQuestion();
+//echo $question->display();
+
+//Récupérer le nombre de questions en DB
+$size = Question::getQuestionsSize();
+echo $size;
