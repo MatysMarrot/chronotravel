38eab7d4fc7538762c13a2bb180cf5889d5d0c4c
MatysMarrot <marrotm38@gmail.com>
2024-01-16 14:29:39 +0100

    Redirection to landing page when you are disconnected

--- controler/teacher.studentstat.ctrl.php
+++ controler/teacher.studentstat.ctrl.php
@@ -3,8 +3,11 @@
 include_once(__DIR__."/../framework/view.class.php");
 
 $view = new View();
-$outgoing = "teacher.statStudent.view.php";
+$outgoing = "../view/teacher.statStudent.view.php";
 
+if(!isset($_SESSION["id"])) {
+    $outgoing = "../controler/landing.ctrl.php";
+}
 $view->display("../view/".$outgoing);
 
 ?>
