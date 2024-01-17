08cdb60940d6acd6187aa4029468329739a7cae0
Yaniissou <yanissoumc@gmail.com>
2024-01-17 16:04:48 +0100

    i wish it was ok

--- view/board.view.php
+++ view/board.view.php
@@ -3,16 +3,46 @@
     <head>
       <meta charset="UTF-8">
       <title>Chrono Travel- Gestion des élèves</title>
-      <link rel="stylesheet" type="text/css" href="../view/style/board.style.css">   
+      <link rel="stylesheet" type="text/css" href="../view/style/board.style.css">
+        <style>
+            #canvas-container {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                z-index: 1;
+            }
+
+            #myCanvas {
+                display: none;
+                border: 1px solid #000;
+            }
+
+            #content {
+                z-index: 2;
+                position: relative;
+                /* Ajoutez d'autres styles pour votre contenu ici */
+            }
+        </style>
     </head>
 
+
+
+
+    <body>
         <table id="board">
 
         </table>
 
+        <div id="canvas-container">
+            <button onclick="showCanvas()">Afficher Canvas</button>
+            <canvas id="myCanvas"></canvas>
+        </div>
 
-    <body>  
-
+        <div id="content">
+            <!-- Votre contenu ici -->
+        </div>
         <script src="./board.ctrl.js"></script>
     </body>
 </html>
