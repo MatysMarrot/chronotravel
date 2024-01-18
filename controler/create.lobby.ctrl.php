18b31f91fe96c52140ccade0f6eac514bb7f6284
Mazine <mazine.meriche@etu.univ-grenoble-alpes.fr>
2024-01-18 15:53:44 +0100

    création d'une partie

---
+++ controler/create.lobby.ctrl.php
@@ -0,0 +1,22 @@
+<?php
+include_once(__DIR__ . "/../controler/utils/Utils.php");
+include_once(__DIR__ . "/../model/DAO.class.php");
+include_once(__DIR__ . "/../model/Student.class.php");
+include_once(__DIR__ . "/../model/Party.class.php");
+
+session_start();
+if(!isset($_SESSION['id']) || $_SESSION['roleid'] != 1){
+    include(__DIR__ . "/../controler/landing.ctrl.php");
+}
+else{
+    $party = new Party(Student::readStudent($_SESSION['id']));
+
+    $party->create();
+    include(__DIR__ . "/../controler/student.lobby.ctrl.php");
+
+}
+
+
+
+
+?>
