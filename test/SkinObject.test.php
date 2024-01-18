ed9e611651482856c911d41e02977e18911ab953
MatysMarrot <marrotm38@gmail.com>
2024-01-18 11:57:41 +0100

    Add possessed and unpossessed skin

--- test/SkinObject.test.php
+++ test/SkinObject.test.php
@@ -7,7 +7,13 @@ var_dump(SkinObject::getAllSkinObjects());
 var_dump(SkinObject::getCurrentSkinOfPlayer(107));
 var_dump(SkinObject::getCurrentSkinOfPlayer(109));
 
-var_dump(SkinObject::isPossessedBy(1, 109));
-var_dump(SkinObject::isPossessedBy(2, 109));
+$skin1 = new SkinObject(1, "Pantalon bleu", 1000, "blue-pants.png", 4);
+$skin2 = new SkinObject(2, "Cheveux court brun", 1000, "brown-hair.png", 2);
+
+var_dump($skin1->isPossessedBy(109));
+var_dump($skin2->isPossessedBy(109));
+
+var_dump(SkinObject::getAllPossessedSkin(109));
+var_dump(SkinObject::getAllunpossessedSkin(109));
 
 ?>
