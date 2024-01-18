acf2ec87df8d2b2d536535651f5623d5a327e834
Yaniissou <yanissoumc@gmail.com>
2024-01-18 12:02:34 +0100

    started parties

--- model/Party.class.php
+++ model/Party.class.php
@@ -4,16 +4,19 @@ require_once(__DIR__."/enum/era.enum.php");
 
 class Party{
 
-    
+    private int $partyid;
+    private int $ownerid;
     private bool $ingame = FALSE; // bool permettant de savoir si le jeu est en cours
     private array $studentPosition; //Sous la forme Student1 => Case1, Student2 => Case2 etc ...
-    private array $students; // liste des élèves
-    private Student $owner; // élève qui a lancé la partie
+    private array $subscribers; // liste des élèves
     private string $code; // code de la game
     private int $id; // laisser la BD gérer
     private Era $era; // thème du plateau courant
 
-    public function __construct(Student $owner){
+    public function __construct(int $partyid,int $ownerid){
+        $this->id = $partyid;
+        $this->subscribers = array();
+        $this->ownerId = $owner;
         $this->owner = $owner;
     }
 
