335161c9bfa2353643e53458587a4ffebca54342
Xavier Jayet-Griffon <xavier.jayet-griffon@etu.univ-grenoble-alpes.fr>
2024-01-19 14:44:18 +0100

    fixe rules prof

--- view/header.teacher.viewpart.php
+++ view/header.teacher.viewpart.php
@@ -6,6 +6,7 @@
             <ul class="ul_horizontal">
                 <li ><a <?= ($currentPage == 'home') ? 'class="active"' : ''; ?> href="../controler/teacher.home.ctrl.php">Accueil</a></li>
                 <li><a <?= ($currentPage == 'manage') ? 'class="active"' : ''; ?> href="../controler/teacher.manage.ctrl.php">Mes Classes</a></li>
+                <li><a <?= ($currentPage == 'rules') ? 'class="active"' : ''; ?> href="../controler/rules.ctrl.php">Règles</a></li>
                 <li><a <?= ($currentPage == 'contact') ? 'class="active"' : ''; ?> href="../controler/contact.ctrl.php">Contact</a></li>
                 <li><a href="../logout.php">Déconnexion</a></li>
             </ul>
