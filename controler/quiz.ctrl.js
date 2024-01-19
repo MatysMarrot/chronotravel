cb77b5a6b7c390e42a5ef880441f1dfaa1b3a6b7
duchosaj <jolan.duchosal@etu.univ-grenoble-alpes.fr>
2024-01-19 11:19:52 +0100

    ajout abstract mini jeu, et changement du quiz.ctrl.js pour obtenir les label et question

--- controler/quiz.ctrl.js
+++ controler/quiz.ctrl.js
@@ -1,22 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var answerCells = document.querySelectorAll(".answer");
-    var selectedAnswerText = document.getElementById("selectedAnswerText");
+import AbstractMinijeu from "../model/js/AbstractMinijeu.mjs";
 
-    answerCells.forEach(function(cell) {
-        cell.addEventListener("click", function() {
-            //réinitialise la couleur de fond de toutes les cellules à chaque nouveau click
-            answerCells.forEach(function(cell) {
-                cell.style.backgroundColor = "#0059FF";
-            });
+class QuizController extends AbstractMinijeu {
+    constructor() {
+        super();
 
-            //change la couleur de fond de la cellule cliquée (orange)
-            this.style.backgroundColor = "#FF8C00";
+        // récupère les labels et la question
+        const labels = this.getLabels();
+        const question = this.getQuestion();
 
-            //obtient le texte de la réponse sélectionnée dans le label
-            var selectedAnswer = this.querySelector(".answer label").innerText;
+        // affiche la question dans la console
+        console.log(question);
 
-            // maj du texte affiché
-            selectedAnswerText.innerText = selectedAnswer;
+        //affiche chaque réponse dans la console (boucle qui parcours tous les label)
+        labels.forEach(function(label) {
+            console.log(label.innerText);
         });
-    });
-});
+
+        document.addEventListener("DOMContentLoaded", function() {
+            var answerCells = document.querySelectorAll(".answer");
+            var selectedAnswerText = document.getElementById("selectedAnswerText");
+
+            answerCells.forEach(function(cell) {
+                cell.addEventListener("click", function() {
+                    //réinitialise la couleur de fond de toutes les cellules à chaque nouveau click
+                    answerCells.forEach(function(cell) {
+                        cell.style.backgroundColor = "#0059FF";
+                    });
+
+                    //change la couleur de fond de la cellule cliquée (orange)
+                    this.style.backgroundColor = "#FF8C00";
+
+                    //obtient le texte de la réponse sélectionnée dans le label
+                    var selectedAnswer = this.querySelector(".answer label").innerText;
+
+                    // maj du texte affiché
+                    selectedAnswerText.innerText = selectedAnswer;
+                });
+            });
+        });
+    }
+}
+
+// crée une instance du contrôleur
+const quizController = new QuizController();
