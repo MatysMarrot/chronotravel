5dd2f02a558e74581243f714e74affc82bc518da
Yaniissou <yanissoumc@gmail.com>
2024-01-19 17:35:57 +0100

    Finished QuestionPacket

---
+++ serveurs/QuestionPacket.class.php
@@ -0,0 +1,114 @@
+<?php
+
+require_once(__DIR__ . '/Packet.abstract.php');
+require_once(__DIR__ . '/../model/Question.class.php');
+require_once(__DIR__ . '/../model/enum/era.enum.php');
+
+
+
+class QuestionPacket extends Packet
+{
+    private array $players;
+    private $data;
+    private Player $player;
+
+    // casté en INT Position/31/4
+
+    public function handle()
+    {
+        // TODO: Implement handle() method.
+    }
+
+    public function __construct(int $pid, array $players)
+    {
+        parent::__construct(-1, $pid);
+        $this->players = $players;
+        $this->questions = array();
+        foreach ($players as $player){
+            for ($i = 0; $i < 10; $i++) {
+                $this->questions[$player->getId()][] = Question::getRandomQuestionByEra(Era::MODERN_AGES/**EnumUtils::$ENUM_ORDER[$player->getPosition() / (31 / 4)]**/);
+            }
+        }
+
+
+
+    }
+
+
+    public function stringify(): string
+    {
+        $this->data = [
+            "partyid" => self::getPartyid(),
+            "cid" => $this->player->getId(),
+            "questions" => [
+            ]
+
+        ];
+
+        foreach ($this->questions as $question) {
+            $questionData = [
+                "id" => $question->getId(),
+                "content" => $question->getContent(),
+                "reponses" => [],
+            ];
+
+            foreach ($question->getAnswers() as $reponse) {
+                $reponseData = [
+                    "id" => $reponse->getId(),
+                    "questionId" => $reponse->getQuestionId(),
+                    "content" => $reponse->getContent(),
+                    "right" => $reponse->isCorrect(),
+                ];
+
+                $questionData['reponses'][] = $reponseData;
+            }
+
+            $this->data['questions'][] = $questionData;
+        }
+
+
+
+        return json_encode($this->data);
+    }
+
+    public function stringifyPlayers(): array
+    {
+        $playersData = [];
+
+        foreach ($this->players as $player) {
+            $playerData = [
+                "partyid" => self::getPartyid(),
+                "cid" => $player->getId(),
+                "questions" => [],
+            ];
+
+            foreach ($this->questions[$player->getId()] as $question) {
+                $questionData = [
+                    "id" => $question->getId(),
+                    "content" => $question->getContent(),
+                    "reponses" => [],
+                ];
+
+                foreach ($question->getAnswers() as $reponse) {
+                    $reponseData = [
+                        "id" => $reponse->getId(),
+                        "questionId" => $reponse->getQuestionId(),
+                        "content" => $reponse->getContent(),
+                        "right" => $reponse->isCorrect(),
+                    ];
+
+                    $questionData['reponses'][] = $reponseData;
+                }
+
+                $playerData['questions'][] = $questionData;
+            }
+
+            $playersData[] = json_encode($playerData);
+        }
+
+        return $playersData;
+    }
+
+}
+
+?>
--- test/Login.test.php
+++ test/Login.test.php
@@ -1,6 +1,9 @@
 <?php 
 include_once(__DIR__."/../model/DAO.class.php");
 include_once(__DIR__."/../model/Student.class.php");
+include_once(__DIR__."/../model/enum/era.enum.php");
+
 
 echo password_verify("Motdepasse1", password_hash("Motdepasse1", PASSWORD_BCRYPT)) ? "True" : "False";
 
+echo EraUtil::getOrdinal(Era::ANTIQUITY);
