fdb17031cc56351348d45196d80515b044f92caf
Mazine <mazine.meriche@etu.univ-grenoble-alpes.fr>
2024-01-20 15:58:59 +0100

    fix lobby

--- controler/create.lobby.ctrl.php
+++ controler/create.lobby.ctrl.php
@@ -3,16 +3,26 @@ include_once(__DIR__ . "/../controler/utils/Utils.php");
 include_once(__DIR__ . "/../model/DAO.class.php");
 include_once(__DIR__ . "/../model/Student.class.php");
 include_once(__DIR__ . "/../model/Party.class.php");
+include_once(__DIR__ . "/../framework/view.class.php");
+
+$view = new View();
 
 session_start();
 if(!isset($_SESSION['id']) || $_SESSION['roleid'] != 1){
-    include(__DIR__ . "/../controler/landing.ctrl.php");
+    $view->display("../controler/landing.ctrl.php");
 }
 
 else{
-    $party = new Party($_SESSION['id']);
-    $party->create();
-    include(__DIR__ . "/../controler/student.lobby.ctrl.php");
+    $dao = DAO::get();
+    $data = [$_SESSION['id']];
+    $query = "SELECT creatorid FROM party WHERE creatorid = ? AND partystate = 1";
+    $table = $dao->query($query,$data);
+
+    if(count($table) == 0){
+        $party = new Party($_SESSION['id']);
+        $party->create();
+    }
+    $view->display("../controler/student.lobby.ctrl.php");
 
 }
 
