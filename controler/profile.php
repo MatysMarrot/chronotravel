4c62de560ec3525caa567f608fdf5b6b6a05de45
ATIK Reda <atikr@hostname.iut2.upmf-grenoble.fr>
2024-01-20 16:01:08 +0100

    personnage mis en place

--- controler/profile.php
+++ controler/profile.php
@@ -3,35 +3,95 @@ include_once(__DIR__."/../framework/view.class.php");
 include_once(__DIR__."/../model/DAO.class.php");
 include_once(__DIR__."/../model/Student.class.php");
 include_once(__DIR__."/utils/Utils.php");
+include_once(__DIR__."/../model/SkinObject.class.php");
 
 $view = new View();
 session_start();
 
 $error = "";
 
-// Vérifier si l'utilisateur est connecté
 if (isset($_SESSION['id'])) {
     $userId = $_SESSION['id'];
 
-    // Récupérer les informations de l'utilisateur depuis la base de données
     $dao = DAO::get();
     $data = [$userId];
-    $query = "SELECT login FROM Person WHERE id = ?";
-    $table = $dao->query($query, $data);
+    $queryUser = "SELECT id, login FROM Person WHERE id = ?";
+    $resultUser = $dao->query($queryUser, $data);
 
-    if (count($table)) {
-        $pseudo = $table[0]['login'];
+    if (count($resultUser) > 0) {
+        $pseudo = $resultUser[0]['login'];
+        $userId = $resultUser[0]['id'];
+
+        $view->assign("pseudo", $pseudo);
+        $view->assign("userId", $userId);
+
+        $queryClass = "SELECT classid FROM studentclass WHERE studentid = ?";
+        $resultClass = $dao->query($queryClass, [$userId]);
+
+        if (count($resultClass) > 0) {
+            $classId = $resultClass[0]['classid'];
+
+            $queryClassName = "SELECT name FROM Class WHERE id = ?";
+            $resultClassName = $dao->query($queryClassName, [$classId]);
+
+            if (count($resultClassName) > 0) {
+                $className = $resultClassName[0]['name'];
+
+                // Nouvelle requête pour récupérer le nom du professeur
+                $profQuery = "SELECT Person.name FROM Person
+                              JOIN ClassTeacher ON Person.id = ClassTeacher.teacherId
+                              WHERE ClassTeacher.classId = ?";
+                $profResult = $dao->query($profQuery, [$classId]);
+
+                if (count($profResult) > 0) {
+                    $profName = $profResult[0]['name'];
+                    $view->assign("profName", $profName);
+                } else {
+                    $error = "Nom du professeur non trouvé pour la classe";
+                }
+
+                $view->assign("className", $className);
+            } else {
+                $error = "Nom de la classe non trouvé pour l'étudiant";
+            }
+        } else {
+            $error = "Classe non trouvée pour l'étudiant";
+        }
     } else {
-        // Gérer le cas où l'utilisateur n'est pas trouvé
         $pseudo = "Utilisateur inconnu";
     }
-
-    $view->assign("pseudo", $pseudo); // Assurez-vous que cette variable est disponible dans votre vue
 } else {
-    // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
     header("Location: login.view.php");
     exit();
 }
+
+function getCurrentSkinOfPlayer(int $playerId) : array {
+    $dao = DAO::get();
+    $query = "SELECT hat, hair, teeshirt, pants, shoes FROM currentskin WHERE playerid=?";
+    $table = $dao->query($query, [$playerId]);
+    $currentSkin = [];
+    if(!count($table)) {
+        $dao->exec("INSERT INTO currentskin VALUES ($playerId, null, null, null, null, null, '000000')");
+        $currentSkin = [null, null, null, null, null];
+    } else {
+        $row = $table[0];
+        for($i=0; $i < 5; $i++) {
+            if($row[$i] != null) {
+                $query = "SELECT * FROM skinobject WHERE skinid=?";
+                $table = $dao->query($query, [$row[$i]]);
+                $rowSkinObject = $table[0];
+                $newSkin = new SkinObject($rowSkinObject["skinid"], $rowSkinObject["name"], $rowSkinObject["price"], $rowSkinObject["location"], $rowSkinObject["parts"]);
+                $currentSkin[] = $newSkin;
+            } else {
+                $currentSkin[] = null;
+            }
+        }
+    }
+    return $currentSkin;
+}
+
+$currentSkin = getCurrentSkinOfPlayer($userId);
+$view->assign("currentSkin", $currentSkin);
 
 $view->assign("error", $error);
 $view->display("profile.php");
--- view/header.student.viewpart.php
+++ view/header.student.viewpart.php
@@ -1,3 +1,33 @@
+<?php
+    session_start();
+
+    if (isset($_SESSION['id'])) {
+        $userId = $_SESSION['id'];
+
+       
+        require_once(__DIR__."/../model/DAO.class.php");
+
+        $dao = DAO::get();
+        $data = [$userId];
+        $query = "SELECT currency FROM Person WHERE id = ?";
+        $result = $dao->query($query, $data);
+
+   
+        if ($result !== false && count($result) > 0) {
+            $currencyAmount = $result[0]['currency'];
+            
+        } else {
+           
+            echo "Erreur lors de la récupération du montant de la monnaie.";
+        }
+    } else {
+    
+        header("Location: login.view.php");
+        exit();
+    }
+?>
+
+
 <head>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" type="text/css" href="../view/style/header.student.css">
@@ -13,8 +43,8 @@
                 <a href="profile.php"><i class="material-symbols-outlined">Person</i></a>
                 <span class="tooltip_text">MON PROFIL</span>
             </li>
-            <li <?= ($currentPage == 'checkroom') ? 'class="active"' : ''; ?>>
-                <a href="../controler/checkroom.ctrl.php"> <i class="material-symbols-outlined">Checkroom</i></a>
+            <li <?= ($currentPage == 'outfit') ? 'class="active"' : ''; ?>>
+                <a href="outfit.php"> <i class="material-symbols-outlined">Checkroom</i></a>
                 <span class="tooltip_text">MES TENUES</span>
             </li>
             <li <?= ($currentPage == 'logout') ? 'class="active"' : ''; ?>>
@@ -25,6 +55,6 @@
     </nav>
     <div id="number_chronocoin">
         <img src="../view/img/chrono_coin.png" alt="ChronoCoins">
-        <span>12345</span>
+        <span><?php echo $currencyAmount; ?></span>
     </div>
 </header>
