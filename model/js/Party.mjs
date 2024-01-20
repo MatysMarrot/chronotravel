56f8070fb944c313d03ba3b8df54d9c43d1f3c37
Elisée Chemin <Elink.chem@gmail.com>
2024-01-20 21:07:14 +0100

    Added handlePacketfunction, updatePlayerPosition and declare winner

--- model/js/Party.mjs
+++ model/js/Party.mjs
@@ -3,13 +3,18 @@ import "./Board.mjs";
 import {Board} from "./Board.mjs";
 import {Tableau} from "./Tableau.mjs";
 import {Player} from "./Player.mjs";
+import {VictoryPacket} from "./packets/VictoryPacket.js";
+import {MovementPacket} from "./packets/MovementPacket.mjs";
 export class Party{
     board;
     id;
     owner;
     players;
+    over = false;
+    inMiniJeux = false;
+    socket;
 
-    constructor(board, json) {
+    constructor(board, json, socket) {
         /**
          * FORME DU JSON:
          * {
@@ -23,7 +28,6 @@ export class Party{
          *     }
          *
          * }
-         *
          */
 
         //console.log(json);
@@ -34,8 +38,11 @@ export class Party{
 
         this.players = [];
         for (const joueurs of json.players){
-            this.players.push(new Player(joueurs));
+            this.players[joueurs.id] = new Player(joueurs);
+            //this.players.push();
         }
+
+        this.socket = socket;
     }
 
     get id() {
@@ -54,12 +61,71 @@ export class Party{
         return this.board;
     }
 
-    startMinigame(canva, minigame){
-        //TODO :
+    handlePacket(stringedData){
+        try (let data){
+            data = JSON.parse(stringedData);
+        } catch (error){
+            throw new TypeError("Could not parse data !");
+        }
+
+        let packet;
+        //On identifie le type de packet recu !
+        switch (data.action) {
+            case "victory":{
+                packet = new VictoryPacket(this, data);
+            } break;
+            case "movement":{
+                packet = new MovementPacket(this, data);
+            } break;
+
+            case "minigame":{
+
+            } break;
+
+        }
+
+        //TODO : verifier la classe
+        if (!this.over && !this.inMiniJeux)
+        packet.handle(this);
     }
 
-    updatePlayerPosition(positionsPacket){
-        //TODO :
+    updatePlayerPosition(playersMovement){
+        for (let p of playersMovement){
+            //On cherche le joueur associé
+            let playerObject = this.players[p.key];
+
+            //Si on ne le trouve pas on passe au suivant
+            if (playerObject == null){
+                continue;
+            }
+
+            playerObject.move(p.value);
+        }
+    }
+
+    declareWinner(winners){
+        //TODO : PROPER VICTORY SCREEN AND MESSAGE
+        if (winners == null  || winners.length === 0){
+            alert("It's too bad, but no one won...");
+            return;
+        }
+
+        this.over = false;
+        switch (winners.length) {
+            case 1: {
+                alert("Le gagnant est " + winners.at(0));
+            } break;
+
+            default: {
+                let msg = 'Les gagants sont ';
+                for (let i = 0; i < winners.length-2; i++){
+                    msg += winners.at(i) + ", ";
+                }
+
+                msg += winners.at(winners.length -1) + " ! ";
+                alert(msg);
+            }
+        }
     }
 
     drawPlayerPosition(){
