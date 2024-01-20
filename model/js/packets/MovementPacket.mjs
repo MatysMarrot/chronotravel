2594bbbd05149778680122a3fb03c430ea1b4281
Elisée Chemin <Elink.chem@gmail.com>
2024-01-20 21:01:14 +0100

    Added Victory, Movement and Abstract packet types

---
+++ model/js/packets/MovementPacket.mjs
@@ -0,0 +1,20 @@
+import {AbstractPacket} from "../AbstractPacket.mjs";
+import "../Party.mjs";
+
+export class MovementPacket extends AbstractPacket {
+
+    playersMovement;
+    constructor(Partie, data) {
+        super(data.id, data.partyId);
+        this.playersMovement = [];
+
+        for (let players of data.players){
+            this.playersMovement[players.id] = players.movement;
+        }
+
+    }
+
+    handle(partie){
+        partie.updatePlayerPosition(this.playersMovement);
+    }
+}
---
+++ model/js/packets/VictoryPacket.js
@@ -0,0 +1,30 @@
+import {AbstractPacket} from "../AbstractPacket.mjs";
+import "../Party.mjs";
+export class VictoryPacket extends AbstractPacket{
+
+    winners = [];
+
+    constructor(Party, data) {
+        super(-1, Party.id);
+
+        for (let player of data.players){
+            if (Party.players[player.id]){
+                this.winners.push(Party.players[player.id].login);
+            }
+        }
+    }
+
+    get winners(){
+        return this.winners;
+    }
+
+    handle(partie){
+        partie.declareWinner(this.winners);
+    }
+
+
+
+
+
+
+}
