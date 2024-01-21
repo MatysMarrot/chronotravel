f819b975ac4d85731525a8b968fd46bc192ebc3d
Xavier Jayet-Griffon <xavier.jayet-griffon@etu.univ-grenoble-alpes.fr>
2024-01-21 11:30:49 +0100

    fixe home css

--- view/home.view.php
+++ view/home.view.php
@@ -1,34 +1,33 @@
 <!DOCTYPE html>
-  <html lang="fr">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>ChronoTravel - Accueil</title>
-      <link rel="stylesheet" href="./style.css">
-    </head>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChronoTravel - Accueil</title>
+    <link rel="stylesheet" href="../view/style/style.css">
+</head>
 
-    <body>
+<body>
 
-    <?php
-    $currentPage = 'home';
-    include(__DIR__.'/header.student.viewpart.php');
-    ?>
+<?php
+$currentPage = 'home';
+include(__DIR__.'/header.student.viewpart.php');
+?>
 
-    <main id="home">
-        <h1>Chronotravel</h1>
-        <p>Embarquez pour un voyage dans le temps avec notre jeu de plateau multijoueur. Relevez des défis éducatifs
-            et explorez les époques. Prêt à devenir le maître de l'histoire?</p>
-        <section>
-            <ul class="menu-home">
-                <li ><a class="button-home" href="../controler/create.lobby.ctrl.php">Créer une partie</a></li>
-                <li ><a class="button-home" href="../controler/student.join.ctrl.php">Rejoindre une partie</a></li>
-                <li ><a class="button-home" href="../controler/student.join.classgroup.ctrl.php">Rejoindre une classe</a></li>
-            </ul>
-        </section>
-        <p>Vous ne savez pas comment jouer ? <a href="../controler/rules.ctrl.php">Cliquez ici !</a></p>
+<main id="home">
+    <h1>Chronotravel</h1>
+    <p>Embarquez pour un voyage dans le temps avec notre jeu de plateau multijoueur. Relevez des défis éducatifs
+        et explorez les époques. Prêt à devenir le maître de l'histoire?</p>
+    <section>
+        <ul class="menu-home">
+            <li ><a class="button-home" href="../controler/create.lobby.ctrl.php">Créer une partie</a></li>
+            <li ><a class="button-home" href="../controler/student.join.ctrl.php">Rejoindre une partie</a></li>
+            <li ><a class="button-home" href="../controler/student.join.classgroup.ctrl.php">Rejoindre une classe</a></li>
+        </ul>
+    </section>
+    <p>Vous ne savez pas comment jouer ? <a href="../controler/rules.ctrl.php">Cliquez ici !</a></p>
 
-    </main>
+</main>
 
-
-    </body>
-  </html>
+</body>
+</html>
