8ab937f6d2f0b659913c541f6dbaf988727c48ec
Xavier Jayet-Griffon <xavier.jayet-griffon@etu.univ-grenoble-alpes.fr>
2024-01-21 17:30:09 +0100

    home teacher v1

--- view/teacher.home.view.php
+++ view/teacher.home.view.php
@@ -14,11 +14,12 @@ $currentPage = 'home';
 include(__DIR__.'/header.student.viewpart.php');
 ?>
 
-<aside id="home_button">
+<main id="home_button">
+    <p>Observez les statistiques de vos élèves</p>
     <ul>
         <li id="join_class_button"><a href="../controler/teacher.manage.ctrl.php">Gestion de vos classes</a></li>
     </ul>
-</aside>
+</main>
 
 </body>
 </html>
