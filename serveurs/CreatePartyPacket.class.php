27c505665f77c5525ca323cdbb11459263843997
Mazine <mazine.meriche@etu.univ-grenoble-alpes.fr>
2024-01-22 14:09:20 +0100

    changement packet create party

--- serveurs/CreatePartyPacket.class.php
+++ serveurs/CreatePartyPacket.class.php
@@ -2,55 +2,43 @@
 
 require_once(__DIR__ . '/Packet.abstract.php');
 require_once(__DIR__ . '/enums/Action.enum.php');
-require_once(__DIR__ . '/../model/Student.class.php');
 
-
-class CreatePartyPacket extends Packet
-{
+class CreatePartyPacket extends Packet{
 
     private Action $action;
     private int $id;
     private int $partyId;
-    private int $owner;
     private array $players;
-
-    public function __construct(int $id = -1, int $partyId, int $owner, array $players)
+    private $data;
+    private int $owner;
+    public function __construct(array $players,$pid)
     {
-        $this->action = Action::CREATE;
-        $this->id = $id;
-        $this->partyId = $partyId;
-        $this->owner = $owner;
+        parent::__construct(-1, $pid);
         $this->players = $players;
-        parent::__construct($this->id, $this->partyId);
-
 
     }
+
+    public function stringify() : string{
+
+        $this->data = [
+            "action" => "create",
+            "id" => -1,
+            "partyId" => $this->pid,
+            "owner" => -1,
+            "players" => []
+        ];
+
+        foreach($this->players as $player){
+            $this->data;
+        }
+
+        return $this->data;
+    }
+
 
     public function handle()
     {
         // TODO: Implement handle() method.
     }
-
-    public function stringify()
-    {
-        $this->data = [
-            "action" => $this->action->value,
-            "id" => $this->id,
-            "partyid" => self::getPartyid(),
-            "owner" => $this->owner,
-            "players" => [
-            ]
-
-        ];
-
-        foreach ($this->players as $p) {
-            $this->data['players'][] = [
-                "id" => $p->getId(),
-                "login" => Student::readStudent($p->getId())->getLogin()
-            ];
-
-        }
-    }
 }
-
 ?>
