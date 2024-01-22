38a00b20ba1b3ce219a0946d78ab185ab84bdb3c
Yaniissou <yanissoumc@gmail.com>
2024-01-22 10:34:10 +0100

    Packets

--- serveurs/CreatePartyPacket.class.php
+++ serveurs/CreatePartyPacket.class.php
@@ -2,47 +2,22 @@
 
 require_once(__DIR__ . '/Packet.abstract.php');
 require_once(__DIR__ . '/enums/Action.enum.php');
-require_once(__DIR__ . '/../model/Party.class.php');
 
 class CreatePartyPacket extends Packet{
 
     private Action $action;
+    private int $id;
     private int $partyId;
     private int $owner;
-    private Party $party;
-    public function __construct(Party $party)
+    public function __construct($data)
     {
-        parent::__construct(-1,$party->getId());
-        $this->party = $party;
+        parent::__construct($data['cid'],$data['partyId']);
 
 
     }
     public function handle()
     {
         // TODO: Implement handle() method.
-    }
-
-    public function stringify()
-    {
-        $infos = [
-            "action" => "create",
-            "id" => -1,
-            "partyid" => $this->getPartyid(),
-            "owner" => -1,
-            "players" => [
-            ]
-
-        ];
-
-        foreach ($this->party->getPlayers() as $p) {
-            $infos['players'][] = [
-                "id" => $p->getId(),
-                "login" => Student::readStudent($p->getId())->getLogin()
-            ];
-
-        }
-
-        return $infos;
     }
 }
 ?>
