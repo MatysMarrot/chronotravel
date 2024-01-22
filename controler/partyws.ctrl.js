d60fa16d3a169284902e8764471a2f955a72e70a
Yanissou <yanissoumc@gmail.com>
2024-01-22 22:45:37 +0100

    reformat

--- controler/partyws.ctrl.js
+++ controler/partyws.ctrl.js
@@ -1,23 +1,23 @@
 import {Party} from "../model/js/Party.mjs";
 import {PlayerJoinsPacket} from "../model/js/packets/PlayerJoinsPacket.mjs";
-import {retreiveSession} from "../controler/utils/jsUtils.mjs";
+import {retreiveSession, retrieveSessionFromDiv} from "../controler/utils/jsUtils.mjs";
+
 
 // Créer une connexion WebSocket
 const socket = new WebSocket("ws://192.168.14.112:1414");
-
+const session = retrieveSessionFromDiv();
 let partie = null;
 // La connexion est ouverte
 socket.addEventListener("open", function (event) {
-    retreiveSession().then(function (result){
-        let packet = new PlayerJoinsPacket(result.id, result.partyId);
-        packet.handle(socket);
-    });
+    let packet = new PlayerJoinsPacket(session.id, session.partyId);
+    packet.handle(socket);
+
 });
 
 
 // Écouter les messages
 socket.addEventListener("close", function (event) {
-    if (partie != null && partie.isOver){
+    if (partie != null && partie.isOver) {
         alert("La partie est terminée !");
     }
     console.log("Connexion avec le serveur fermée: ", event.data);
@@ -37,7 +37,7 @@ socket.addEventListener("message", function (event) {
     let info;
     try {
         info = JSON.parse(event.data);
-    } catch (error){
+    } catch (error) {
         console.log("Could not parse " + event);
         return;
     }
@@ -45,13 +45,13 @@ socket.addEventListener("message", function (event) {
     //Si pas d'action dans la data
     //Ne doit pas arriver mais on est prudent ici
     console.log("Valeur de action : " + info.action);
-    if (info.action == null){
+    if (info.action == null) {
         console.log("data.action EST NULL");
         return;
     }
 
     //Si c'est un packet create
-    if (info.action === "create" && partie == null){
+    if (info.action === "create" && partie == null) {
         console.log("CREATION OBJET PARTIE");
         partie = new Party(document.getElementById("board"), info, socket);
         console.log(partie);
