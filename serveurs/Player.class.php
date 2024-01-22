f55cea86cf54f5e302f5f70df5593446f1ee6c8f
Yanissou <yanissoumc@gmail.com>
2024-01-22 20:33:16 +0100

    Player Position relation

--- serveurs/Player.class.php
+++ serveurs/Player.class.php
@@ -1,45 +1,31 @@
 
 <?php
 
-    class Player{
+    class Position{
 
-        private Student $student;
-        private int $pid; //Party ID
+        private int $id;
         private int $position; //Position sur le board
         private string $lastpacket;
         private int $currentMovement;
 
-        public function __construct(Student $student,int $pid, $position = 0, $lastpacket = "")
+        public function __construct(int $id, $position = 0, $lastpacket = "")
         {
-            $this->student = $student;
-            $this->pid = $pid;
+            $this->id = $id;
             $this->position = $position;
             $this->lastpacket = $lastpacket;
             $this->currentMovement = 0;
         }
 
-        /**
-         * @return string
-         */
-        public function getLogin(): string
-        {
-            return $this->student->getLogin();
-        }
 
-        /**
-         * @return Student
-         */
-        public function getStudent(): Student
-        {
-            return $this->student;
-        }
+
+
 
         /**
          * @return int
          */
         public function getId(): int
         {
-            return $this->student->getId();
+            return $this->id;
         }
 
         /**
