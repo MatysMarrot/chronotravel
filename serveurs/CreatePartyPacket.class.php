b8648353c896b11a6b5ca8748f0bdf5a061c049f
Mazine <mazine.meriche@etu.univ-grenoble-alpes.fr>
2024-01-22 14:09:20 +0100

    changement packet create party

--- serveurs/CreatePartyPacket.class.php
+++ serveurs/CreatePartyPacket.class.php
@@ -8,13 +8,34 @@ class CreatePartyPacket extends Packet{
     private Action $action;
     private int $id;
     private int $partyId;
+    private array $players;
+    private $data;
     private int $owner;
-    public function __construct($data)
+    public function __construct(array $players,$pid)
     {
-        parent::__construct($data['cid'],$data['partyId']);
-
+        parent::__construct(-1, $pid);
+        $this->players = $players;
 
     }
+
+    public function stringify() : string{
+
+        $this->data = [
+            "action" => "create",
+            "id" => -1,
+            "partyId" => $this->pid,
+            "owner" => -1,
+            "players" => []
+        ];
+
+        foreach($this->players as $player){
+            $this->data;
+        }
+
+        return $this->data;
+    }
+
+
     public function handle()
     {
         // TODO: Implement handle() method.
