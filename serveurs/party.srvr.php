cd69babad6ea1a8040ba04fb449818db9ae84fcd
Mazine <mazine.meriche@etu.univ-grenoble-alpes.fr>
2024-01-22 11:50:12 +0100

    fix require once action

--- serveurs/party.srvr.php
+++ serveurs/party.srvr.php
@@ -3,6 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../model/DAO.class.php';
 require_once __DIR__ . '/../model/Party.class.php';
 require_once __DIR__ . '/../model/Student.class.php';
+require_once(__DIR__ . '/enums/Action.enum.php');
 //require_once __DIR__ . '/../model/Waitingroom.class.php'; May require another room
 
 /**
@@ -60,7 +61,7 @@ class PartyImpl implements MessageComponentInterface{
 
     function onMessage(ConnectionInterface $conn, $msg)
     {
-        //echo sprintf("New message from '%s': %s\n", $conn->resourceId, $msg);
+        echo sprintf("New message from '%s': %s\n", $conn->resourceId, $msg);
 
         $decoded = json_decode($msg, true);
 
@@ -68,6 +69,7 @@ class PartyImpl implements MessageComponentInterface{
             return;
         }
         if ($decoded['action'] == Action::JOIN->value) {
+            echo sprintf("Received '%s' from %s\n",$decoded['action'], $conn->resourceId);
 
             $packet = new JoinPacket($decoded['id'],$decoded['partyId']);
             $this->clientIdConn[$decoded['id']] = $conn;
