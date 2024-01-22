05e7dba39272373f6e56af28c98b95a83523d066
Mazine <mazine.meriche@etu.univ-grenoble-alpes.fr>
2024-01-22 09:58:11 +0100

    suppression de la vérif

--- controler/create.lobby.ctrl.php
+++ controler/create.lobby.ctrl.php
@@ -13,37 +13,6 @@ if(!isset($_SESSION['id']) || $_SESSION['roleid'] != 1){
 }
 
 else{
-    $dao = DAO::get();
-    $data = [$_SESSION['id']];
-    $query = "SELECT creatorid FROM party WHERE creatorid = ? AND partystate = 1";
-    $table = $dao->query($query,$data);
-
-    if(count($table) == 0){
-        $party = new Party($_SESSION['id']);
-        $party->create();
-    }
-    $view->display("../controler/student.lobby.ctrl.php");
-
-    foreach ($table as $ligne){
-        if (isset($ligne['id'])){
-            $partyid = $ligne['id'];
-
-            $query = "DELETE FROM PartyCode WHERE partyId = ?";
-            $dao->exec($query, [$partyid]);
-
-            $query = "DELETE FROM PartyStudent WHERE partyId = ?";
-            $dao->exec($query, [$partyid]);
-
-            $query = "DELETE FROM Party WHERE id = ?";
-            $dao->exec($query, [$partyid]);
-
-            var_dump($partyid);
-        }
-    }
-
-
-
-
     $party = new Party($_SESSION['id']);
     $party->create();
     $view->display("../controler/student.lobby.ctrl.php");
