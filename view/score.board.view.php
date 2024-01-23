e02d8e768b673f62a984edf6341b33c46817888a
Xavier Jayet-Griffon <xavier.jayet-griffon@etu.univ-grenoble-alpes.fr>
2024-01-23 22:52:39 +0100

    nouveau score board (affiche les skin des joueurs

--- view/score.board.view.php
+++ view/score.board.view.php
@@ -8,16 +8,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /></head>
 </head>
 
+<body id="scoreBody">
 <a id="button-return" href="../controler/home.ctrl.php"><i class="material-symbols-outlined">arrow_back</i></a>
-<body class="relativ">
-    <table id="score">
-        <thead>
-        <tr >
-            <th>Position</th><th>Pseudo</th><th>Gains</th>
-        </tr>
-        </thead>
-        <tbody>
         <?=$tableau?>
-        </tbody>
-    </table>
 </body>
