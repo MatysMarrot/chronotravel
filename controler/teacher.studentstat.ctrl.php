2fa425b95d1e76472091a420899cb65a3c894acb
MatysMarrot <marrotm38@gmail.com>
2024-01-23 10:47:47 +0100

    Add stat view of student for teacher

--- controler/teacher.studentstat.ctrl.php
+++ controler/teacher.studentstat.ctrl.php
@@ -1,12 +1,20 @@
 <?php
-
 include_once(__DIR__."/../framework/view.class.php");
+include_once(__DIR__."/../model/Student.class.php");
+include_once(__DIR__."/../model/Stat.class.php");
+include_once(__DIR__."/../model/StatPerGame.class.php");
 
 $view = new View();
 $outgoing = "../view/teacher.statStudent.view.php";
 
 if(!isset($_SESSION["id"])) {
     $outgoing = "../controler/landing.ctrl.php";
+}
+$view->assign("student", $student);
+$allStat = Stat::getStatOf($student->getId());
+if ($allStat !=null) {
+    $allStatJSON = json_encode($allStat);
+    $view->assign("allStatJSON", $allStatJSON);
 }
 $view->display("../view/".$outgoing);
 
--- view/teacher.statStudent.view.php
+++ view/teacher.statStudent.view.php
@@ -1,40 +1,35 @@
 <!DOCTYPE html>
-  <html lang="fr">
+<html lang="fr">
     <head>
       <meta charset="UTF-8">
       <title>Chrono Travel- Gestion des élèves</title>
       <link rel="stylesheet" type="text/css" href="../view/style/style.css">    
     </head>
-
     <body>
         <?php $currentPage = 'manage';
         include(__DIR__.'/header.teacher.viewpart.php'); ?>
-
-
-    <main>
+        <main class="teacherStat">
             <h1>Statistiques</h1>
-            <h2><?=$student->getFirstname() . " ".$student->getLastname()?> (<?=$student->getLogin()?>)</h2>
-            <table class="data">     
-                <tr>
-                    <td>Nombre de parties jouées :</td>                             
-                    <td>1512</td>
-                </tr>    
-                <tr>
-                    <td>Taux de bonnes réponses :</td>                             
-                    <td>9999%</td>
-                </tr>    
-                <tr>
-                    <td>Points forts :</td>                             
-                    <td>Epoque sup à 75% de reussite</td>
-                </tr>    
-                <tr>
-                    <td>Difficultés :</td>                             
-                    <td>Epoque inf à 50% de reussite</td>
-                </tr>    
-            </table>  
+            <h2><?=$student->getFirstname(). " ".$student->getLastname()?> (<?=$student->getLogin()?>)</h2>
+            <div class="game">
+                <?php if ($allStat != null): ?>
+                    <h3>Parties jouées : <?= $allStat->getGamePlayed() ?></h3>
+                    <h3>Parties gagnées : <?= $allStat->getGameWin() ?></h3>
+                <?php else: ?>
+                    <h3>Aucune statistique existante pour ce joueur</h3>
+                <?php endif; ?>
+            </div>
+            <div id="chartDiv"></div>
         </main>
-
-        <!--Footer --> 
-        <?php include(__DIR__.'/footer.viewpart.html'); ?>   
+        <?php include(__DIR__.'/footer.viewpart.html'); ?>
+        <?php if ($allStat != null): ?>
+            <div class="json" style="display: none">
+                <?= $allStatJSON ?>
+            </div>
+        <?php endif; ?>
     </body>
+    <?php if ($allStat != null): ?>
+        <script src="https://code.jscharting.com/2.9.0/jscharting.js"></script>
+        <script src="../view/js/chart.js"></script>
+    <?php endif; ?>
 </html>
