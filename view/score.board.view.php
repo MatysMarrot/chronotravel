82511d34d4b297bef906be353d74d3613f787535
Xavier Jayet-Griffon <xavier.jayet-griffon@etu.univ-grenoble-alpes.fr>
2024-01-23 17:12:03 +0100

    reste à gérer l'égalité au niveau des gains + début css

--- view/score.board.view.php
+++ view/score.board.view.php
@@ -1,3 +1,13 @@
+<head>
+    <style>
+        #qcm {
+            background-image: url('view/img/theme/2-moyenage/4-img-moyenage.jpg');
+        }
+    </style>
+    <link rel="stylesheet" href="../view/style/style.css">
+
+</head>
+
 <table id="score">
     <thead>
     <tr >
