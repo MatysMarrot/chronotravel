d0c9e563d2540e27443b2f8ed954873b7de21636
duchosaj <jolan.duchosal@etu.univ-grenoble-alpes.fr>
2024-01-24 09:39:23 +0100

    dys mode fini

--- view/js/dyslexieFont.js
+++ view/js/dyslexieFont.js
@@ -1,12 +1,9 @@
 const divDys = document.querySelector('.dys').textContent;
 const body = document.body;
-var button = document.querySelectorAll('button');
 
 // Vérifier si la classe est déjà présente sur le paragraphe
 if (divDys === "1") {
     body.classList.add('dyslexic-font')
-    button.classList.add('dyslexic-font');
 } else {
     body.classList.remove('dyslexic-font')
-    button.classList.remove('dyslexic-font');
 }
