1a10fbe6cc41a8e2f8c6d6b379fea87dd239eccd
MatysMarrot <marrotm38@gmail.com>
2024-01-25 10:04:41 +0100

    Delete commentary

--- model/js/Tableau.mjs
+++ model/js/Tableau.mjs
@@ -33,14 +33,6 @@ export class Tableau {
         }
     }
 
-    //TODO :
-    /*window.addEventListener('resize', function () {
-        if (document.getElementById("mytables").style.display === "block") {
-            resizetables();
-            drawtables();
-        });
-    }*/
-
     initTable() {
         let starCells = [];
         for (let i = 0; i < LINES; i++) {
@@ -82,43 +74,7 @@ export class Tableau {
                 this.listeCellules.push(row.cells[row.cells.length - 1]);
             }
         }
-        /*
-        // Ajoute la classe "board_avalaible" aux cellules d'une ligne sur 2
-                for (let ligne = 0; ligne < this.table.rows.length; ligne += 2) {
-                    let row = this.table.rows[ligne];
 
-                    for (let coll = 0; coll < row.cells.length; coll++) {
-                        row.cells[coll].classList.add('board_available');
-                    }
-                }
-
-        // Ajoute les cellules "descendantes" du chemin
-                let dg = false;
-                let position = 0;
-                for (let i = 1; i < this.table.rows.length; i += 2) {
-                    let row = this.table.rows[i];
-
-                    (dg ? position = 0 : position = row.cells.length - 1);
-                    let cellule = row.cells[position];
-
-                    cellule.classList.add('board_available');
-                    dg = !dg;
-                }
-
-        // Attribue des identifiants uniques à chaque cellule
-                let id = 0;
-                for (const cellules of starCells) {
-                    if (!cellules.classList.contains("board_available")) {
-                        continue;
-                    }
-
-                    cellules.id = id;
-                    cellules.textContent = id;
-                    id++;
-                    this.listeCellules.push(cellules);
-                }
-            }
-        */
     }
     clear() {
         let i = 0;
