26afa71232bf02e5b4a96018499f0f35425167f0
duchosaj <jolan.duchosal@etu.univ-grenoble-alpes.fr>
2024-01-25 11:48:52 +0100

    unknown header fini

--- view/header.unknown.viewpart.php
+++ view/header.unknown.viewpart.php
@@ -1,6 +1,6 @@
-<a href="../controler/landing.ctrl.php"><img id="logo" src="../view/img/logo.png" alt="ChronoTravel_logo"></a>
+
 <header class="connectionpage">
-        <a <?= ($currentPage == 'rules') ? 'class="active"' : ''; ?> href="../controler/rules.ctrl.php">Règles</a>
-        <a href="../controler/landing.ctrl.php"><h1>Chronotravel</h1></a>
-        <a <?= ($currentPage == 'contact') ? 'class="active"' : ''; ?> href="../controler/contact.ctrl.php">Contact</a>
+    <a <?= ($currentPage == 'rules') ? 'class="active"' : ''; ?> href="../controler/rules.ctrl.php">Règles</a>
+    <a href="../controler/landing.ctrl.php"><img id="header_unknown_logo" src="../view/img/logo.png" alt="ChronoTravel"></a>
+    <a <?= ($currentPage == 'contact') ? 'class="active"' : ''; ?> href="../controler/contact.ctrl.php">Contact</a>
 </header>
