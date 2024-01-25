464efcf88ae9784ea75bdb2c54d49e352f030d1d
MatysMarrot <marrotm38@gmail.com>
2024-01-25 14:46:42 +0100

    fix stat to correspond with the game

--- test/Stat.test.php
+++ test/Stat.test.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/../model/Stat.class.php');
 require_once(__DIR__ . '/../model/StatPerGame.class.php');
 
-$myStat = Stat::getStatOf(109);
+$myStat = Stat::getStatOf(114);
 var_dump($myStat);
 ?>
 
