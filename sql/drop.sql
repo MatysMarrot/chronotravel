7a7f46cbae74eef144ea029a7945d31cfd958187
Yanissou <yanissoumc@gmail.com>
2024-01-25 21:07:04 +0100

    added cascade on drop

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,28 +1,28 @@
 -- Drop les foreign key
 
-DROP TABLE IF EXISTS public.answers;
-DROP TABLE IF EXISTS public.questions;
-DROP TABLE IF EXISTS public.history;
-DROP TABLE IF EXISTS public.partycode;
-DROP TABLE IF EXISTS public.party;
-DROP TABLE IF EXISTS public.stat;
+DROP TABLE IF EXISTS public.answers CASCADE;
+DROP TABLE IF EXISTS public.questions CASCADE;
+DROP TABLE IF EXISTS public.history CASCADE;
+DROP TABLE IF EXISTS public.partycode CASCADE;
+DROP TABLE IF EXISTS public.party CASCADE;
+DROP TABLE IF EXISTS public.stat CASCADE;
 
 
 -- Drop les tables
-DROP TABLE IF EXISTS public.theme;
-DROP TABLE IF EXISTS public.partystate;
-DROP TABLE IF EXISTS public.classteacher;
-DROP TABLE IF EXISTS public.studentclass;
-DROP TABLE IF EXISTS public.class;
-DROP TABLE IF EXISTS public.person;
-DROP TABLE IF EXISTS public.role;
+DROP TABLE IF EXISTS public.theme CASCADE;
+DROP TABLE IF EXISTS public.partystate CASCADE;
+DROP TABLE IF EXISTS public.classteacher CASCADE;
+DROP TABLE IF EXISTS public.studentclass CASCADE;
+DROP TABLE IF EXISTS public.class CASCADE;
+DROP TABLE IF EXISTS public.person CASCADE;
+DROP TABLE IF EXISTS public.role CASCADE;
 
 /**-- Drop des sequences
-DROP SEQUENCE IF EXISTS public.user_id_seq;
-DROP SEQUENCE IF EXISTS public.answers_id_seq;
-DROP SEQUENCE IF EXISTS public.class_id_seq;
-DROP SEQUENCE IF EXISTS public.party_id_seq;
-DROP SEQUENCE IF EXISTS public.partystate_id_seq;
-DROP SEQUENCE IF EXISTS public.questions_id_seq;
-DROP SEQUENCE IF EXISTS public.role_id_seq;
-DROP SEQUENCE IF EXISTS public.theme_id_seq;**/
+DROP SEQUENCE IF EXISTS public.user_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS public.answers_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS public.class_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS public.party_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS public.partystate_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS public.questions_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS public.role_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS public.theme_id_seq CASCADE;**/
