ec46ebad4ffee2fe1e5c62c109c9debd87383377
Elisée Chemin <elisee.chemin@etu.univ-grenoble-alpes.fr>
2024-01-26 15:44:44 +0100

    PlayerJoinsPacket comments

--- model/js/packets/PlayerJoinsPacket.mjs
+++ model/js/packets/PlayerJoinsPacket.mjs
@@ -1,12 +1,15 @@
 import {AbstractPacket} from "../AbstractPacket.mjs";
 
 export class PlayerJoinsPacket extends AbstractPacket {
+    //Documentation dans packet.doc.md
+    //Packet ENVOYE au debut de la partie pour notifier sa présence au serveur
 
     constructor(id, partyid) {
         super(id, partyid);
     }
 
     handle(socket){
+        //Forme du packet comme définie dans la doc
         let data = {
             "action": "join",
             "id": this.id,
@@ -15,6 +18,7 @@ export class PlayerJoinsPacket extends AbstractPacket {
 
         //console.log("Join data: " + JSON.stringify(data));
 
+        //Envoie le packet au serveur
         socket.send(JSON.stringify(data));
     }
 
