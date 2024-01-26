bfe966eac7329823f7136725450f52d736bb8c8f
Elisée Chemin <elisee.chemin@etu.univ-grenoble-alpes.fr>
2024-01-26 15:46:23 +0100

    MovementPacket.mjs comments

--- model/js/packets/MovementPacket.mjs
+++ model/js/packets/MovementPacket.mjs
@@ -2,15 +2,19 @@ import {AbstractPacket} from "../AbstractPacket.mjs";
 //import {Party} from "../model/js/Party.mjs";
 
 export class MovementPacket extends AbstractPacket {
+    //Documentation dans packet.doc.md
+    //Packet contenant la quantité de case de laquelle chaque joueur doit avancer
 
     playersMovement;
     constructor(partie, data) {
         super(-1, data.partyId);
 
+        //On crée une nouvelle map ([id, nbr de cases]
         this.playersMovement = new Map();
 
         for (let players of data.players){
-            //console.log(players.id,players.movement);
+            //Pour chaque joueur dans la packet
+            //On map le contenu
             this.playersMovement.set(players.id, players.movement);
         }
 
@@ -18,6 +22,7 @@ export class MovementPacket extends AbstractPacket {
     }
 
     handle(partie){
+        //Appelle la fonction qui modifie les positions des joueurs
         partie.updatePlayerPosition(this.playersMovement);
     }
 }
