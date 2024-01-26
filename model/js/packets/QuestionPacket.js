14786148eb39fe9e7379198c62d143df188ed330
Elisée Chemin <elisee.chemin@etu.univ-grenoble-alpes.fr>
2024-01-26 15:43:31 +0100

    VictoryPacket.js comments

--- model/js/packets/QuestionPacket.js
+++ model/js/packets/QuestionPacket.js
@@ -3,11 +3,14 @@ import {Question} from "../Question.mjs";
 
 export class QuestionPacket extends AbstractPacket {
 
+    //Documentation dans packet.doc.md
+
     questions = [];
     data;
 
     constructor(Party, data) {
         super(-1, Party.id);
+        //Construit des questions d'après les questions passé par le serveur (voir Question.mjs)
         for (let q of data.questions){
             this.questions.push(new Question(q))
         }
@@ -15,6 +18,7 @@ export class QuestionPacket extends AbstractPacket {
     }
 
     handle(partie){
+        //Decale le début du mini jeu
         setTimeout(() => partie.startMinigame(this), 3000);
     }
 
