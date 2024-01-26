87f4a3bf1d61a1c89c78fb556355a0394d0e84f1
Elisée Chemin <elisee.chemin@etu.univ-grenoble-alpes.fr>
2024-01-26 15:42:31 +0100

    VictoryPacket.js comments

--- model/js/packets/VictoryPacket.js
+++ model/js/packets/VictoryPacket.js
@@ -2,11 +2,14 @@ import {AbstractPacket} from "../AbstractPacket.mjs";
 //import {Party} from "../model/js/Party.mjs";
 export class VictoryPacket extends AbstractPacket{
 
+    //Documentation dans packet.doc.md
+
     winners = [];
 
     constructor(Party, data) {
         super(-1, Party.id);
 
+        //On récupère les logins en fonction des id transmits.
         for (let winner of data.winners){
             //console.log(winner);
                 this.winners.push(Party.players.get(winner.id).login);
@@ -18,6 +21,7 @@ export class VictoryPacket extends AbstractPacket{
     }
 
     handle(partie){
+        //Appel de la fonction de gestion des gagnants.
         partie.declareWinner(this.winners);
     }
 
